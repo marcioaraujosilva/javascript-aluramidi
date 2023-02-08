8b7aafa5a470b1d23f03cc726affde135151309f
Marcio Araujo Silva <115511600+marcioaraujosilva@users.noreply.github.com>
2023-02-08 14:20:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function tocaSom(idElementoAudio)
+{
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+for (let contador = 0; contador < listaDeTeclas.length; contador++) 
+{
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (event) {
+        if(event.code === 'Enter' || event.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
